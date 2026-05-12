9e441414fb69c932c351ad93f3df96dea1ed98cd
LIPKA <curoae4@gmail.com>
2026-05-12 16:17:31 +0800

    upd

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VV-Site/',
+  base: "/VV-Site/",
   plugins: [
     react(),
     tailwindcss(),
